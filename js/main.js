2528303d929ef0a4cc28feec4faed3341d38f15d
Nookons <Nookons@users.noreply.github.com>
2021-03-05 02:26:18 +0200

    ChangeProject

--- js/main.js
+++ js/main.js
@@ -1,72 +1,48 @@
-var product = $('.product-slide');
-product.owlCarousel({
-    items:4,
-    loop:true,
-    margin:0,
-    autoplay:true,
-    autoplayTimeout:750,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:4
+document.getElementById('modalNExt').addEventListener('click', skeepModal);
+
+function skeepModal () {
+    let modalProg = document.querySelector('.progress-bar-modal');
+    let ModalCheck = document.getElementById('ModalCheck');
+
+
+    if(ModalCheck.checked !== false) {
+
+        modalProg.classList.add('active');
+
+        setTimeout(hideModal, 3500);
+
+        function hideModal() {
+            let modal = document.getElementById('modal');
+            modal.style.display = "none";
         }
     }
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[2500])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
-
-
-var inst = $('.inst-slide');
-inst.owlCarousel({
-    items:5,
-    loop:true,
-    margin: 1,
-    autoplay:false,
-    autoplayTimeout:500,
-    autoplayHoverPause:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        700:{
-            items: 3
-        },
-        1000:{
-            items:4
-        },
-        1500:{
-            items:5
-        },
-        2000:{
-            items:7
-        }
+    else {
+        return false;
     }
-});
+}
+window.addEventListener('resize', function AutoSizeSlide() {
+    let UserY = window.innerHeight;
+    let Head  = document.getElementById('header');
+    let Slide = document.querySelectorAll('.item')
 
-let burgerMenu = document.querySelector('.burger-menu');
-let menu = document.querySelector('.menu');
-let burger = document.getElementById('burger');
+    var MathSlideY = UserY - Head.offsetHeight;
 
-burger.addEventListener('click', function () {
-    burger.classList.toggle ('active');
-    menu.classList.toggle   ('active');
+    Slide.forEach(item => {
+        item.style.height = UserY + 'px';
+    })
 })
 
-window.addEventListener('resize', function () {
-    const x = document.documentElement.clientWidth;
+function StartSizeSlide() {
+    let UserY = window.innerHeight;
+    let Head  = document.getElementById('header');
+    let Slide = document.querySelectorAll('.item')
 
-    if (x > 1080) {
-        burger.classList.remove ('active');
-        menu.classList.remove('active');
-    }
-})
+    var MathSlideY = UserY - Head.offsetHeight;
+
+    Slide.forEach(item => {
+        item.style.height = UserY + 'px';
+    })
+}
+
+StartSizeSlide();
+
